ab750a8b0e9b781583abe510380624d97e9e771a
中里潤貴 <haku@nakasatojuntakanoMac-mini.local>
2017-10-01 07:44:15 +0900

    [add] リスト操作用要素追加

--- src/js/_vue/list.vue
+++ src/js/_vue/list.vue
@@ -5,9 +5,27 @@
 
 <template>
   <div class="pageList">
+    <div class="addList">
+      <div class="addList__input">
+        <input type="text" placeholder="リスト名を入力してください">
+      </div>
+      <p class="addList__submit">
+        <button>リストの作成</button>
+      </p>
+    </div>
     <ul class="list">
-      <li v-for="item in data">
-        {{item.name}}
+      <li v-for="item in data" class="list__item">
+        <div class="listDetail">
+          <p class="listDetail__title">
+            <a href="#" class="listDetail__title__linkText">
+              {{item.name}}
+            </a>
+            <button class="listDetail__title__editBtn">編集</button>
+          </p>
+          <div class="listDetail__delete">
+            <button class="listDetail__delete__btn">削除</button>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -15,6 +33,7 @@
 
 <script>
   export default {
+    methods: {},
     computed: {
       data () {
         return this.$store.state.data
